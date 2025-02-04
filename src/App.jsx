6f0c2493f137bdd1dd1b5a06b65a193c2d10ad9e
fazalshah007 <bilalfazal52@gmail.com>
2025-02-05 00:41:05 +0500

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+import Sidebar from "./components/Sidebar"
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  
+
+  return (
+    <>
+    <Dashboard />
+    {/* <Navbar /> */}
+    {/* <Sidebar /> */}
+    </>
+  )
+}
+
+export default App
